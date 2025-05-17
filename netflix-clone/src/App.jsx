01e1d8dca3d52dc87ba455ec89f6569cfb321afe
haimi-gebregiorgis <haimanotwolde1@gmail.com>
2025-05-17 15:28:38 -0400

    header and footer components

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,7 +1,13 @@
+
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <><h1>Hello</h1></>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
